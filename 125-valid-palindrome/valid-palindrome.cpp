41e96df2077bb1873ab6a9807750a509d8bb11b5
Harsh Vardhan Tomar <harshvardhantomar2202@gmail.com>
2026-09-08 20:39:57 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (7.65%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,24 +1,44 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        for(int i=0;i<s.length();){
-            s[i]=tolower(s[i]);
-            if(!isalnum(s[i])){
-                s.erase(i,1);
-            }
-            else{
-                i++;
+        // for(int i=0;i<s.length();){
+        //     s[i]=tolower(s[i]);
+        //     if(!isalnum(s[i])){
+        //         s.erase(i,1);
+        //     }
+        //     else{
+        //         i++;
+        //     }
+        // }
+        // int n=s.length();
+        // string ans=s;
+        // int i=0,j=n-1;
+        // while(i<j){
+        //     swap(s[i],s[j]);
+        //     i++;
+        //     j--;
+        // }
+        // if(ans==s){
+        //     return true;
+        // }
+        // return false;
+
+
+        string str="";
+        for(int i=0;i<s.length();i++){
+            if(isalnum(s[i])){
+                str+=tolower(s[i]);
             }
         }
-        int n=s.length();
-        string ans=s;
+        string ans=str;
+        int n=str.length();
         int i=0,j=n-1;
         while(i<j){
-            swap(s[i],s[j]);
+            swap(str[i],str[j]);
             i++;
             j--;
         }
-        if(ans==s){
+        if(ans==str){
             return true;
         }
         return false;
