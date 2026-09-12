5232e1a8dcf946318fc668ce490f58a6b21501d2
Harsh Vardhan Tomar <harshvardhantomar2202@gmail.com>
2026-09-12 21:21:22 +0530

    Time: 11 ms (20.76%) | Memory: 13.9 MB (92.26%) - LeetSync

---
+++ 1168-duplicate-zeros/duplicate-zeros.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        const int n=arr.size();
+        if(n==1){
+            return;
+        }
+        int i=0,j=n-1;
+        while(i<j){
+            int indx=0;
+            if(arr[i]==0){
+                for(int k=j;k>i;k--){
+                    arr[k]=arr[k-1];
+                }
+                arr[i+1]=indx;
+                i=i+2;
+            }
+            else{
+                i++;
+            }
+        }
+    }
+};
