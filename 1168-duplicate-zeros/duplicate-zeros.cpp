9d03572721f03de92864e08d5924ed678b314769
Harsh Vardhan Tomar <harshvardhantomar2202@gmail.com>
2026-09-12 21:42:40 +0530

    Time: 0 ms (100.00%) | Memory: 14.3 MB (27.02%) - LeetSync

--- 1168-duplicate-zeros/duplicate-zeros.cpp
+++ 1168-duplicate-zeros/duplicate-zeros.cpp
@@ -5,19 +5,30 @@ public:
         if(n==1){
             return;
         }
-        int i=0,j=n-1;
-        while(i<j){
-            int indx=0;
+        // int i=0,j=n-1;            // MERA KHUDKA LOGIC HAI YEH WALA
+        // while(i<j){
+        //     int indx=0;
+        //     if(arr[i]==0){
+        //         for(int k=j;k>i;k--){
+        //             arr[k]=arr[k-1];
+        //         }
+        //         arr[i+1]=indx;
+        //         i=i+2;
+        //     }
+        //     else{
+        //         i++;
+        //     }
+        // }
+        vector<int> ans;
+        for(int i=0;i<n;i++){
             if(arr[i]==0){
-                for(int k=j;k>i;k--){
-                    arr[k]=arr[k-1];
-                }
-                arr[i+1]=indx;
-                i=i+2;
+                ans.push_back(0);
             }
-            else{
-                i++;
-            }
+            ans.push_back(arr[i]);
+        }
+        ans.resize(n);
+        for(int i=0;i<n;i++){
+            arr[i]=ans[i];
         }
     }
 };
