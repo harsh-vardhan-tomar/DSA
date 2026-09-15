9e267d8e6a26f417be404e824396d7d31ba15032
Harsh Vardhan Tomar <harshvardhantomar2202@gmail.com>
2026-09-15 18:48:08 +0530

    Time: 28 ms (74.34%) | Memory: 69.3 MB (73.94%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -4,12 +4,12 @@ public:
         
         int ans=0;
         sort(arr.begin(),arr.end());
-        int n=arr.size();
-        for(int i=arr.size()-1;i>=(n-k);i--){
-            ans=arr[i];
-        }
-        return ans;
-        // return arr[arr.size()-k];
+        // int n=arr.size();
+        // for(int i=arr.size()-1;i>=(n-k);i--){
+        //     ans=arr[i];
+        // }
+        // return ans;
+        return arr[arr.size()-k];
 
 
     }
