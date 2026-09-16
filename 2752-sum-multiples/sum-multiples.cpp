d193f1d04b7ecedd38be42397d51745ef31a3d20
Harsh Vardhan Tomar <harshvardhantomar2202@gmail.com>
2026-09-16 09:54:14 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (49.63%) - LeetSync

--- 2752-sum-multiples/sum-multiples.cpp
+++ 2752-sum-multiples/sum-multiples.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     int sumOfMultiples(int n) {
-        vector<int> arr(n+1);
-        for(int i=1;i<n+1;i++){
-            arr[i]=i;
-        }
+        
         int sum=0;
         for(int i=0;i<=n;i++){
-            if(arr[i]%3==0 || arr[i]%5==0 || arr[i]%7==0){
-                sum+=arr[i];
+            if(i%3==0 || i%5==0 || i%7==0){
+                sum+=i;
             }
         }
         return sum;
