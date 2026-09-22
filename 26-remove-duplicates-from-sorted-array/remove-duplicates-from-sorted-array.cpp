321d9656ed43c6f4e7773e334f4bb7dcb9dfb4a9
Harsh Vardhan Tomar <harshvardhantomar2202@gmail.com>
2026-09-22 18:52:03 +0530

    Time: 0 ms (100.00%) | Memory: 22.5 MB (80.31%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -9,21 +9,32 @@ public:
         if(n==1){
             return 1;
         }
-        int i=0,j=n-1;
-        // vector<int> ans(n);
-        // ans[0]=arr[0];
-        while(i<j){
-            if(arr[i]==arr[i+1]){
-                for(int k=i;k<j;k++){
-                    arr[k]=arr[k+1];
-                }
-                j--;
-                duplicate++;
+        // int i=0,j=n-1;
+        // while(i<j){
+        //     if(arr[i]==arr[i+1]){
+        //         for(int k=i;k<j;k++){
+        //             arr[k]=arr[k+1];
+        //         }
+        //         j--;
+        //         duplicate++;
+        //     }
+        //     else{
+        //         i++;
+        //     }
+        // }
+        // return n-duplicate; 
+        int nondup=0;
+        int j=1;
+        while(j<n){
+            int i=j-1;
+            if(arr[i]!=arr[j]){
+                nondup++;
+                arr[nondup]=arr[j];
             }
-            else{
-                i++;
-            }
+            j++;
         }
-        return n-duplicate; 
+        return nondup+1;
+        
+        
     }
 };
