4238bdeecedcf05054e0c971c81ac4726f42e5aa
Harsh Vardhan Tomar <harshvardhantomar2202@gmail.com>
2026-09-25 23:13:35 +0530

    Time: 0 ms (100.00%) | Memory: 36.3 MB (44.18%) - LeetSync

--- 4418-number-of-intersecting-interval-pairs-i/number-of-intersecting-interval-pairs-i.cpp
+++ 4418-number-of-intersecting-interval-pairs-i/number-of-intersecting-interval-pairs-i.cpp
@@ -4,7 +4,7 @@ public:
         int n=arr.size();
         int intersect=0;
         for(int i=0;i<n;i++){
-            for(int j=i+1;j<=n-1;j++){
+            for(int j=i+1;j<n;j++){
                 int maxi=max(arr[i][0],arr[j][0]);
                 int mini=min(arr[i][1],arr[j][1]);
                 if(maxi<=mini){
