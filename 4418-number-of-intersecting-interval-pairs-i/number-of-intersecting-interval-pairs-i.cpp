7a73c7cf3fef3e4777d711f4ea5339a0fca8d5e9
Harsh Vardhan Tomar <harshvardhantomar2202@gmail.com>
2026-09-25 22:53:34 +0530

    Time: 8 ms (20.78%) | Memory: 36.4 MB (44.18%) - LeetSync

---
+++ 4418-number-of-intersecting-interval-pairs-i/number-of-intersecting-interval-pairs-i.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int countIntersectingIntervals(vector<vector<int>>& arr) {
+        int n=arr.size();
+        int intersect=0;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<=n-1;j++){
+                int maxi=max(arr[i][0],arr[j][0]);
+                int mini=min(arr[i][1],arr[j][1]);
+                if(maxi<=mini){
+                    intersect++;
+                }
+            }
+        }
+        return intersect;
+    }
+};
